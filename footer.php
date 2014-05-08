10cf84b361f8022593c1e16d7f5c4f25aae870e4
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-05-08 14:09:01 -0400

    added GA

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-	</div><!-- #main -->
+  </div><!-- #main -->
 
-	</div><!-- #container -->
+  </div><!-- #container -->
 
-	<footer id="footer">
-    &copy; 2012 All Rights Reserved. Hand in Hand for Literacy: Suite 306, 26 Exchange St. E., St. Paul, MN 55101.
+  <footer id="footer">
+    &copy; <?= date('Y'); ?> All Rights Reserved. Hand in Hand for Literacy: Suite 306, 26 Exchange St. E., St. Paul, MN 55101.
     </footer>
 
   <!--[if lt IE 7 ]>
@@ -12,9 +12,20 @@
   <![endif]-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script src="<?php echo bloginfo('template_url'); ?>/js/jquery.flexslider-min.js"></script> 
-	<script src="<?php echo bloginfo('template_url'); ?>/js/jquery.fittext.js"></script>  
+  <script src="<?php echo bloginfo('template_url'); ?>/js/jquery.fittext.js"></script>  
     <script src="<?php echo bloginfo('template_url'); ?>/js/jquery.fitvids.js"></script>  
-	<script src="<?php echo bloginfo('template_url'); ?>/js/hand.js"></script>
+  <script src="<?php echo bloginfo('template_url'); ?>/js/hand.js"></script>
+
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50822302-2', 'handinhandforliteracy.org');
+  ga('send', 'pageview');
+
+</script>
 
 <?php wp_footer(); ?>
 </body>
